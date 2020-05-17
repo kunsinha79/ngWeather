2255d41644742c1542ad3b40ec86d89b888b43a7
kuntala sinha <kunsinha79@gmail.com>
2020-05-18 00:11:03 +0200

    feat(ngWeather): added mat buttons to links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser'
-import { NgModule } from '@angular/core'
-import { HttpClientModule } from '@angular/common/http'
-import { MatIconModule } from '@angular/material/icon'
-import { MatCardModule } from '@angular/material/card'
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatButtonModule } from '@angular/material/button';
 
-import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component'
-import { WeatherInfoComponent } from './components/weather-info/weather-info.component'
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { WeatherInfoService } from './services/weather-info.service'
-import { WeatherCardComponent } from './components/weather-card/weather-card.component'
-import { WeatherErrorComponent } from './components/weather-error/weather-error/weather-error.component'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { WeatherInfoComponent } from './components/weather-info/weather-info.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { WeatherInfoService } from './services/weather-info.service';
+import { WeatherCardComponent } from './components/weather-card/weather-card.component';
+import { WeatherErrorComponent } from './components/weather-error/weather-error/weather-error.component';
 
 @NgModule({
   declarations: [
@@ -28,6 +29,7 @@ import { WeatherErrorComponent } from './components/weather-error/weather-error/
     MatIconModule,
     MatCardModule,
     MatProgressSpinnerModule,
+    MatButtonModule,
   ],
   providers: [WeatherInfoService],
   bootstrap: [AppComponent],
--- src/app/components/weather-info/weather-info.component.spec.ts
+++ src/app/components/weather-info/weather-info.component.spec.ts
@@ -4,21 +4,21 @@ import {
   TestBed,
   fakeAsync,
   tick,
-} from '@angular/core/testing'
+} from '@angular/core/testing';
 
-import { WeatherInfoComponent } from './weather-info.component'
-import { HttpClientTestingModule } from '@angular/common/http/testing'
-import { MatCardModule } from '@angular/material/card'
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { WeatherInfoService } from '../../services/weather-info.service'
-import { Observable, of } from 'rxjs'
-import { IWeather } from 'src/app/shared/weather.type'
-import { currentWeatherMock } from 'src/app/test/mock/current-weather.mock'
+import { WeatherInfoComponent } from './weather-info.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatCardModule } from '@angular/material/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { WeatherInfoService } from '../../services/weather-info.service';
+import { Observable, of } from 'rxjs';
+import { IWeather } from 'src/app/shared/weather.type';
+import { currentWeatherMock } from 'src/app/test/mock/current-weather.mock';
 
 describe('WeatherInfoComponent', () => {
-  let component: WeatherInfoComponent
-  let fixture: ComponentFixture<WeatherInfoComponent>
-  let weatherInfoService: WeatherInfoService
+  let component: WeatherInfoComponent;
+  let fixture: ComponentFixture<WeatherInfoComponent>;
+  let weatherInfoService: WeatherInfoService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -29,35 +29,35 @@ describe('WeatherInfoComponent', () => {
       ],
       declarations: [WeatherInfoComponent],
       providers: [WeatherInfoService],
-    }).compileComponents()
-  }))
+    }).compileComponents();
+  }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(WeatherInfoComponent)
-    component = fixture.componentInstance
-    weatherInfoService = TestBed.get(WeatherInfoService)
+    fixture = TestBed.createComponent(WeatherInfoComponent);
+    component = fixture.componentInstance;
+    weatherInfoService = TestBed.get(WeatherInfoService);
     spyOn(weatherInfoService, 'getCityWeather').and.callFake(
       (): Observable<IWeather> => of(currentWeatherMock)
-    )
+    );
 
-    fixture.detectChanges()
-  })
+    fixture.detectChanges();
+  });
 
   it('should create', () => {
-    expect(component).toBeTruthy()
-  })
+    expect(component).toBeTruthy();
+  });
 
   it('onInit should call getCityWeather', () => {
-    component.ngOnInit()
-    expect(weatherInfoService.getCityWeather).toHaveBeenCalled()
-  })
+    component.ngOnInit();
+    expect(weatherInfoService.getCityWeather).toHaveBeenCalled();
+  });
 
   it('should render 5 cards for weather current information of 5 cities', fakeAsync(() => {
-    fixture.detectChanges()
-    tick()
-    fixture.detectChanges()
-    const bannerElement: HTMLElement = fixture.nativeElement
-    const cards = bannerElement.querySelectorAll('mat-card')
-    expect(cards.length).toBe(5)
-  }))
-})
+    fixture.detectChanges();
+    tick();
+    fixture.detectChanges();
+    const bannerElement: HTMLElement = fixture.nativeElement;
+    const cards = bannerElement.querySelectorAll('mat-card');
+    expect(cards.length).toBe(5);
+  }));
+});
